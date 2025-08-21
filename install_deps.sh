f784989bde26764c9839ecaa116d366751e8be2b
David Linux desktop <tvaarben@gmail.com>
2025-08-21 10:33:35 +0200

    adding to .gitignore file

---
+++ install_deps.sh
@@ -0,0 +1,5 @@
+python -m venv venv
+source venv/bin/activate #on windows: venv\Scripts\activate
+pip install -r requirements.txt
+pip install playwright install
+pip install playwright-deps #only for linux
