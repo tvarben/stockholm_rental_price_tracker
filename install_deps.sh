122bd51ecf9432a3f535552d1dc8e1ae19dd7dfb
David Linux desktop <tvaarben@gmail.com>
2025-08-21 11:26:23 +0200

    updated install script

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 python3 -m venv venv
 source venv/bin/activate #on windows: venv\Scripts\activate
 pip install -r requirements.txt
-playwright install
 playwright install-deps #only for linux
+playwright install
